05c3571c94f926b8d381f403670352a518461cf9
Shirly-lap <mejiaacostashirlipaola@gmail.com>
2024-08-01 11:24:28 -0500

    cracion del login

---
+++ src/controllers/Page.controller.ts
@@ -0,0 +1,23 @@
+import { ILogin } from "../models/ILogin"
+
+export class PageController {
+    url : string
+
+    constructor (url: string){
+        this.url = url
+    }
+
+    async login(data: ILogin, endPoint: string){
+        const response = await fetch(`${this.url}${endPoint}`, {
+            method: 'POST',
+            headers : {
+                'Content-Type' : 'Application/json'
+            },
+            body : JSON.stringify(data)
+        });
+
+        const token = response.json()
+        return token
+    }
+    
+}
